31369f701290f8564b361f52248f8051b643dd5a
NaimaGit <NaimaGit@baoudenenaima@gmail.com>
2014-03-13 11:13:54 +0100

    Ajout de la classe Categorie

---
+++ TpGit/src/Classes/Categorie.java
@@ -0,0 +1,15 @@
+package Classes;
+public class Categorie {
+private Integer idcategorie;
+private String libellecategorie;
+
+public Categorie(Integer wid,String wlib)  {
+idcategorie = wid;
+libellecategorie = wlib;
+}
+public Integer getIdcategorie() {
+return idcategorie;
+}
+public String getLibellecategorie() {
+return libellecategorie;
+}}
